97728ecec68dd54ee99073bccd6a50429ec7ff01
Keigo Kusumegi <kusukun7@gmail.com>
2025-03-11 11:50:35 -0400

    fix works_to_sql

--- BigQuery/OpenAlex/works_to_csv.sql
+++ BigQuery/OpenAlex/works_to_csv.sql
@@ -4,8 +4,8 @@ SELECT
   JSON_VALUE(work, '$.doi') AS doi,
   JSON_VALUE(work, '$.title') AS title,
   JSON_VALUE(work, '$.display_name') AS display_name,
-  JSON_VALUE(work, '$.publication_year') AS publication_year,
-  JSON_VALUE(work, '$.publication_date') AS publication_date,
+  CAST(JSON_VALUE(work, '$.publication_year') AS INT64) AS publication_year,
+  CAST(JSON_VALUE(work, '$.publication_date') AS DATE) AS publication_date,
   JSON_QUERY(work, '$.ids') AS ids,  -- Nested JSON for additional IDs
   JSON_VALUE(work, '$.language') AS language,
   JSON_QUERY(work, '$.primary_location') AS primary_location,  -- Nested JSON for primary location
@@ -13,8 +13,8 @@ SELECT
   JSON_VALUE(work, '$.type_crossref') AS type_crossref,
 
   -- Timestamp fields
-  JSON_VALUE(work, '$.updated_date') AS updated_date,
-  JSON_VALUE(work, '$.created_date') AS created_date,
+  CAST(JSON_VALUE(work, '$.updated_date') AS TIMESTAMP) AS updated_date,
+  CAST(JSON_VALUE(work, '$.created_date') AS TIMESTAMP) AS created_date,
 
   -- Open access and laicense information
   JSON_QUERY(work, '$.open_access') AS open_access,  -- Nested JSON for OA details
@@ -30,11 +30,10 @@ SELECT
   JSON_EXTRACT_ARRAY(work, '$.related_works') AS related_works,
   JSON_EXTRACT_ARRAY(work, '$.referenced_works') AS referenced_works,
   JSON_EXTRACT_ARRAY(work, '$.locations') AS locations,
-  JSON_EXTRACT_ARRAY(work, '$.mesh') AS mesh,
 
   -- Citation metrics
-  JSON_VALUE(work, '$.cited_by_count') AS cited_by_count,
-  JSON_VALUE(work, '$.fwci') AS fwci,  -- Field-weighted citation impact
+  CAST(JSON_VALUE(work, '$.cited_by_count') AS INT64) AS cited_by_count,
+  CAST(JSON_VALUE(work, '$.fwci') AS FLOAT64) AS fwci,  -- Field-weighted citation impact
   JSON_QUERY(work, '$.citation_normalized_percentile') AS citation_normalized_percentile,
   JSON_QUERY(work, '$.cited_by_percentile_year') AS cited_by_percentile_year,
   JSON_EXTRACT_ARRAY(work, '$.counts_by_year') AS counts_by_year,
@@ -50,13 +49,13 @@ SELECT
   JSON_QUERY(work, '$.primary_topic') AS primary_topic,
 
   -- Additional metadata
-  JSON_VALUE(work, '$.has_fulltext') AS has_fulltext,
+  CAST(JSON_VALUE(work, '$.has_fulltext') AS BOOL) AS has_fulltext,
   JSON_VALUE(work, '$.fulltext_origin') AS fulltext_origin,
-  JSON_VALUE(work, '$.is_retracted') AS is_retracted,
-  JSON_VALUE(work, '$.is_paratext') AS is_paratext,
+  CAST(JSON_VALUE(work, '$.is_retracted') AS BOOL) AS is_retracted,
+  CAST(JSON_VALUE(work, '$.is_paratext') AS BOOL) AS is_paratext,
 
   -- abstract_inverted_index
   JSON_QUERY(work, '$.abstract_inverted_index') AS abstract_inverted_index,
 
 FROM 
-  `openalex_202407.works_sample100`  -- change here to the table you want to query
+  `openalex_202503.works`  -- change here to the table you want to query
